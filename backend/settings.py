9afa4c3f6df8bb95d54d49057c0272f95b2f62d2
Alex <alexgenriech10@gmail.com>
2023-01-09 22:32:11 -0800

    Add hosts for api calls

--- backend/settings.py
+++ backend/settings.py
@@ -34,7 +34,8 @@ if os.getenv("DEBUG_MODE") == 'True' or True:
 
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv("FRONT_END_URL")]
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv("FRONT_END_URL")]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
 
 
 # Application warning settings
