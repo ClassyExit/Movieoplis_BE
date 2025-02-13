c4c08e68e4c69e64606b78037839886a484c7677
Alex Genriech <alexgenriech10@gmail.com>
2025-02-13 11:16:30 -0800

    Added DB, refactor code

--- library/urls.py
+++ library/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from . import views
+
 
 #TODO: Add url to main url file (uncomment it)
-urlpatterns = []
+urlpatterns = [
+    path('list', views.ListAPI, name='list'),
+]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def ListAPI(request): 
+
+    if request.method == 'GET':
+        # GET LIST TO USER
+        return Response('GET')
+
+    elif request.method == 'POST':
+        # ADD ITEM TO LIST
+        return Response('POST')
+
+    elif request.method == 'DELETE':
+        # DELETE ITEM FROM LIST
+        return Response('DELETE')
