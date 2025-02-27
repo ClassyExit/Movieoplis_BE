ef6891c6b8711442436d1b13e89f453fea74ee1c
Alex Genriech <alexgenriech10@gmail.com>
2025-02-26 16:54:47 -0800

    User model update

--- authentication/models.py
+++ authentication/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     firebase_uid = models.CharField(max_length=100, unique=True, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    canViewVideos = models.BooleanField(default=False)
 
     
     def __str__(self):
