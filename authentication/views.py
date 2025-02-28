34254dc0768e6feab673e1a7906fdc84dd2fa203
Alex Genriech <alexgenriech10@gmail.com>
2025-02-27 19:55:29 -0800

    Update

--- authentication/views.py
+++ authentication/views.py
@@ -7,11 +7,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import User, UserVideoPermissionQueue
-from .serializers import UserSerializer
+from .serializers import UserSerializer, QueueSerializer
 from common_utils.get_uid_from_request import get_uid_from_request
 
 
-@api_view(['POST', 'DELETE', 'GET'])
+@api_view(['POST', 'DELETE', 'GET', 'PATCH'])
 def AddUser(request):
     if request.method == 'POST':
         # ADD USER TO MATCH FIREBASE UID
@@ -56,6 +56,38 @@ def AddUser(request):
             return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
         return Response({'id': user_info.firebase_uid, 'canViewVideos': user_info.canViewVideos, 'isAdmin': user_info.isAdmin}, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PATCH':
+        # GET USER INFO
+        firebase_uid, error_response = get_uid_from_request(request)
+        if error_response:
+            return error_response
+        
+        # Check if requesting user isAdmin
+        user_info = User.objects.filter(firebase_uid=firebase_uid).first()
+
+        if user_info is not user_info.isAdmin:
+            return Response({'Authorization': 'Invalid permissions'}, status=status.HTTP_404_NOT_FOUND)
+        
+        # Get User uid from body to update permission
+        user_update_uid = request.data['update_uid']
+
+        if user_update_uid:
+            #Update the field
+            update_user_info = User.objects.filter(firebase_uid=user_update_uid).first()
+
+            update_user_info.canViewVideos = True
+            update_user_info.save()
+
+            # Delete the query entry
+            queue_entry = UserVideoPermissionQueue.objects.filter(firebase_uid=user_update_uid).first()
+
+            # Remove the user from the queue
+            queue_entry.delete()
+
+            return Response({'success': 'User approved successfully'}, status=status.HTTP_200_OK)
+        
+        return Response({'error': 'No user found'}, status=status.HTTP_404_NOT_FOUND)
         
 
 
@@ -68,7 +100,7 @@ def ApproveUser(request):
         if error_response:
             return error_response
         
-        # Check if user is admin
+        # Only Admin user can retrieve queue
         user_info = User.objects.filter(firebase_uid=firebase_uid).first()
 
         if user_info is not user_info.isAdmin:
@@ -85,30 +117,18 @@ def ApproveUser(request):
 
     elif request.method == 'POST':
         """
-        Approves a user by updating their canViewVideos field to True
+        Users can submit a ticket to be approved
         """
         firebase_uid, error_response = get_uid_from_request(request)
         if error_response:
             return error_response
         
         try:
-            # Check if the user exists in the queue
-            queue_entry = UserVideoPermissionQueue.objects.filter(firebase_uid=firebase_uid).first()
-            if not queue_entry:
-                return Response({'error': 'User not found in the permission queue'}, status=status.HTTP_404_NOT_FOUND)
-            
-            # Check if the user exists in the User model
-            user = User.objects.filter(firebase_uid=firebase_uid).first()
-            if not user:
-                return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-            
-            # Approve the user
-            user.canViewVideos = True
-            user.save()
-            
-            # Remove the user from the queue
-            queue_entry.delete()
-            
-            return Response({'success': 'User approved successfully'}, status=status.HTTP_200_OK)
+            data = {'firebase_uid': firebase_uid}
+            serializer = QueueSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'success': 'Item added successfully'}, status=status.HTTP_201_CREATED)
+            return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
